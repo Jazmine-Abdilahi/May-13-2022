ca196fbdcecf2e6c21b77e8bcd29b62dd8958683
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2022-05-13 19:56:14 -0500

    Add files via upload

---
+++ May13.js
@@ -0,0 +1,50 @@
+/* Video 16 */
+
+const ordersSet = new Set([
+    'Pasta',
+    'Pizza',
+    'Pizza',
+    'Risotto',
+    'Pasta',
+    'Pizza',
+]);
+console.log(orderSet);
+
+console.log(new Set('Jazmine'));
+
+console.log(orderSet.size);
+console.log(orderSet.has('Pizza'));
+console.log(orderSet.has('Bread'));
+ordersSetSet.add('Garlac bread');
+ordersSetSet.add('Garlac bread');
+ordersSetSet.delete('Risotto');
+// orderSet.Clear();
+console.log(order.set);
+
+for (const order of orderSet) console.log(order);
+
+// Example
+const staff = (['Waiter', 'Chef', 'Waiter', 'Chef', 'Manager', 'Waiter'].size
+);
+
+console.log(new set('jazmineabdilahi').size);
+
+/* Video 17 */
+
+orderpizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+};
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbion, Portugal'));
+
+rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']).set('open', 11).set('close', 23)
+
+
+
+
+
+
